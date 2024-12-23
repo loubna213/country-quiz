734e80204fc22e408570b0dc6e5f27c9d639e4e1
loubna213 <98254998+loubna213@users.noreply.github.com>
2024-12-23 16:04:54 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import App from './App.tsx'
 import QuizProvider from './contexts/QuizContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  
+  <StrictMode>
     <QuizProvider>
       <App />
     </QuizProvider>
- 
+   </StrictMode>
 )
